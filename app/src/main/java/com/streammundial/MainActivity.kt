fa74c8fceb5eb21aa781b29051b0650fa464d70f
Zurdo1010 <zurdozurdito26@gmail.com>
2026-06-10 11:56:49 -0600

    Create MainActivity.kt

---
+++ app/src/main/java/com/streammundial/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.streammundial
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Aquí construiremos la interfaz del reproductor M3U en la siguiente fase
+    }
+}
