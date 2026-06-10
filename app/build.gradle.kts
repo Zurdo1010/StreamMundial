06bb709744b0064fb33cdcb8fc71a68e4ad76998
Zurdo1010 <zurdozurdito26@gmail.com>
2026-06-10 15:12:53 -0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler) // <-- El nuevo conector de Kotlin 2.0
 }
 
 android {
@@ -27,8 +28,17 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    
+    buildFeatures {
+        compose = true
+    }
+    // ¡Adiós al bloque composeOptions que causaba el error!
 }
 
 dependencies {
-    // Aquí agregaremos el reproductor de video más adelante
+    val composeBom = platform("androidx.compose:compose-bom:2024.02.00")
+    implementation(composeBom)
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.activity:activity-compose:1.8.2")
 }
