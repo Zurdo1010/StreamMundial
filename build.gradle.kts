96737bdb0124d7a02b3da38afdff42e2bf7f85f1
Zurdo1010 <zurdozurdito26@gmail.com>
2026-06-10 15:29:10 -0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,47 +1,5 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-}
-
-android {
-    namespace = "com.streammundial"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.streammundial"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    
-    // AQUÍ LE DECIMOS QUE USE EL SISTEMA DE DISEÑO MODERNO
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.8"
-    }
-}
-
-dependencies {
-    // AQUÍ DESCARGAMOS LOS "PINCELES" DE JETPACK COMPOSE
-    val composeBom = platform("androidx.compose:compose-bom:2024.02.00")
-    implementation(composeBom)
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.activity:activity-compose:1.8.2")
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.compose.compiler) apply false
 }
